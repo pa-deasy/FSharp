636295d2e8fbd70ebdd127ff1b70462f68d92178
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-02-13 22:04:12 -0800

    pattern matching

---
+++ PatternMatchingCommandLine/Scratchpad.fsx
@@ -0,0 +1,42 @@
+﻿/////////////////////////////////////////////////////////////
+//Patricks attempt at understanding the problem
+/////////////////////////////////////////////////////////////
+
+#load "PatricksDomain.fs"
+#load "PatricksOperations.fs"
+
+open PatternMatchingCommandLine.PatricksDomain
+open PatternMatchingCommandLine.PatricksOperations
+
+
+let verboseTrue = Verbose Flag.Enabled
+let subDirFalse = Subdir Flag.Disabled
+let orderByName = Order OrderBy.Name
+let optionsList =[ verboseTrue; subDirFalse; orderByName]
+
+verboseTrue |> executeArgument
+
+subDirFalse |> executeArgument
+
+orderByName |> executeArgument
+
+optionsList |> executeArguments
+
+
+/////////////////////////////////////////////////////////////
+//Example from website
+/////////////////////////////////////////////////////////////
+
+#load "Domain.fs"
+#load "Operations.fs"
+
+open PatternMatchingCommandLine.Domain
+open PatternMatchingCommandLine.Operations
+
+
+"/v" |> parseOption defaultOptions 
+
+
+parseCommandLine ["/v"; "/s"] 
+
+parseCommandLine ["/v"; "/k"] 
