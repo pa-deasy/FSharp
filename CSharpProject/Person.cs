e01fb5d2b2fb0f4c202bc06db180ff1f40586f35
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-02-13 22:03:41 -0800

    fsharp in csharp solution

---
+++ CSharpProject/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpProject
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine($"My name is {Name}");
+        }
+    }
+}
